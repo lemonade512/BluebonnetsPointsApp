bdaef8546bbbf443b9d75cbf7d76e8a6a77cd68d
Phillip Lemons <philliplemons512@gmail.com>
2015-12-17 02:01:19 -0600

    Add a basic permissions system

--- main.py
+++ main.py
@@ -8,8 +8,44 @@ app = Flask(__name__)
 
 from models import UserData
 
+from functools import wraps
+
 from google.appengine.api import users
 
+
+# Flask example of login_required
+#from functools import wraps
+#from flask import g, request, redirect, url_for
+#
+#def login_required(f):
+#    @wraps(f)
+#    def decorated_function(*args, **kwargs):
+#        if g.user is None:
+#            return redirect(url_for('login', next=request.url))
+#        return f(*args, **kwargs)
+#    return decorated_function
+
+#TODO allow multiple perms to be passed in through *args
+def require_permission(perm):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            print "IN WRAPPER"
+            user_data = get_current_user_data()
+            if not user_data:
+                return "Must be logged in!!!!"
+
+            if not check_perms(user_data, perm):
+                return "Do not have the following permission: " + str(perm)
+            f(*args, **kwargs)
+        return wrapper
+    return decorator
+
+def check_perms(user_data, perm):
+    if perm in user_data.user_permissions:
+        return True
+
+    return False
 
 def get_current_user_data():
     """ This function gets the current user's UserData or returns None """
@@ -22,10 +58,12 @@ def get_current_user_data():
             # TODO need to create a function that redirects the user to a
             # registration page then redirects back to the page they were on.
             user_data = UserData()
-            user_data.user = user()
+            user_data.user = user
             user_data.user_id = user.user_id()
             user_data.first_name = user.nickname()
             user_data.last_name = "Last"
+            #user_data.user_permissions = ['user', 'officer']
+            user_data.user_permissions = ['user']
             user_data.put()
     else:
         user_data = None
@@ -45,7 +83,7 @@ def admin():
 
 @app.route('/hello')
 def hello():
-    """Return a friendly HTTP greeting."""
+    """ Return a friendly HTTP greeting. """
     user_data = get_current_user_data()
     return render_template("hello.html", user_data=user_data, logout=users.create_logout_url("/"))
 
@@ -54,6 +92,16 @@ def hello():
     #    return "Hello " + user.nickname()
     #else:
     #    return redirect(users.create_login_url(request.url))
+
+# NOTE this is a way for me to do some basic testing of the permissions system
+# NOTE the route decorator must be first so it decorates the function returned by
+# any decorators following.
+@app.route('/hello-perm')
+@require_permission('officer')
+def hello_perm():
+    """ Check if officer then show page. """
+    user_data = get_current_user_data()
+    return render_template("hello.html", user_data=user_data, logout=users.create_logout_url("/"))
 
 
 @app.errorhandler(404)
