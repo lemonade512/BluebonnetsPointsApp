83e43f440fed04cecc383ec345b336071cb4a0a8
Phillip Lemons <philliplemons512@gmail.com>
2016-04-02 03:06:02 -0500

    Only show point exceptions if user is an officer

--- utils.py
+++ utils.py
@@ -12,10 +12,15 @@ def render_jinja_template(name, context=None):
         name: The name of the jinja template to be rendered
         context (dict): A dictionary of values to pass to the template
     """
+    # We want to make sure all templates have this function, but we also
+    # need to avoid circular dependencies so import this here.
+    from permissions import check_perms
+
     template_values = {
         'user_data': UserData.get_current_user_data(),
         'login_url': url_for('login', next=request.path),
         'logout_url': users.create_logout_url("/"),
+        'check_perms': check_perms,
     }
 
     if context != None:
